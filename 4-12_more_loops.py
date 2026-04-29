d9432d1e8bfd4d2ea6fb563ee5d44804b941043f
jango <headrichjan@gmail.com>
2026-04-29 16:16:24 +0800

    added 4-12_more_loops.py

---
+++ 4-12_more_loops.py
@@ -0,0 +1,10 @@
+my_foods = ["pizza", "burger", "pasta", "sushi", "fried chicken"]
+friend_foods = ["salad", "steak", "ramen", "tacos", "grilled fish"]
+
+print("My type of foods:")
+for me_foods in my_foods:
+    print(me_foods.title())
+
+print("\nMy friend's type of foods:")
+for his_foods in friend_foods:
+    print(his_foods.title())
