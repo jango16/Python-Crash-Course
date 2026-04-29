bafface0ba6e92802ff9a907f89faa18b9bcb2e3
jango <headrichjan@gmail.com>
2026-04-29 15:52:19 +0800

    added 4-10_slices.py

---
+++ 4-10_slices.py
@@ -0,0 +1,10 @@
+fav_pizza = ['Greenwhich', 'Pizzahut', 'Albertos',
+             'Margherita', 'Pepperoni', 'Hawaiian']
+print("The first three items in the list are:")
+print(fav_pizza[:3])
+
+print("\nThree items from the middle of the list are:")
+print(fav_pizza[2:5])
+
+print("\nThe last three items in the list are:")
+print(fav_pizza[-3:])
