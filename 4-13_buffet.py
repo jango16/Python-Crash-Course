d779c6d26cac99cace3c4c9705e11df20b866391
jango <headrichjan@gmail.com>
2026-04-29 16:31:35 +0800

    added 4-13_buffet.py

---
+++ 4-13_buffet.py
@@ -0,0 +1,12 @@
+buffet_foods = ("fried chicken", "spaghetti", "steamed rice",
+                "vegetable stir-fry", "grilled fish")
+
+print("Original Foods:")
+for foods in buffet_foods:
+    print(foods.title())
+
+print("\nAltered Foods:")
+buffet_foods = ("banana", "cake", "steamed rice",
+                "vegetable stir-fry", "grilled fish")
+for foods in buffet_foods:
+    print(foods.title())
