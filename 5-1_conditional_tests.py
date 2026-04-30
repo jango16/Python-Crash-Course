b4ec3a4229fd3756395f84712da342e2e165ee17
jango <headrichjan@gmail.com>
2026-04-30 16:04:47 +0800

    added 5-1_conditional_tests.py

---
+++ 5-1_conditional_tests.py
@@ -0,0 +1,33 @@
+car = 'subaru'
+
+# True
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+# False
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs car == 'Toyota'? I predict False.")
+print(car == 'Toyota')
+
+print("\nIs car == 'Honda'? I predict False.")
+print(car == 'Honda')
+
+print("\nIs car == 'Ford'? I predict False.")
+print(car == 'Ford')
+
+print("\nIs car == 'Hyundai'? I predict False.")
+print(car == 'Hyundai')
