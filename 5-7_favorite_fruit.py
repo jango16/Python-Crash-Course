1b0970bcc12c474cb8b09e7c9ceadfcf3f90dff8
jango <headrichjan@gmail.com>
2026-05-03 18:38:54 +0800

    added 5-7_favorite_fruit.py

---
+++ 5-7_favorite_fruit.py
@@ -0,0 +1,16 @@
+fav_fruits = ["apple", "banana", "mango"]
+
+if 'apple' != fav_fruits:
+    print("You really like apples!")
+
+if 'watermelon' != fav_fruits:
+    print("\nYou don't like watermelon!")
+
+if 'mango' != fav_fruits:
+    print("\nYou really like mangoes!")
+
+if 'strawberry' != fav_fruits:
+    print("\nYou don't like strawberries!")
+
+if 'banana' != fav_fruits:
+    print("\nYou really like bananas!")
