9d6ab7f0aaa85a155836dfaf99612613c8894eb9
jango <headrichjan@gmail.com>
2026-05-04 18:37:23 +0800

    added 6-1_person.py

---
+++ 6-1_person.py
@@ -0,0 +1,9 @@
+person_info = {
+    'first_name': 'Jhon',
+    'last_name': 'Geralla',
+    'city': 'Cebu City'
+}
+
+print(person_info['first_name'])
+print(person_info['last_name'])
+print(person_info['city'])
