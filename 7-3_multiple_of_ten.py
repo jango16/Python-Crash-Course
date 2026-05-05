23d95f7f6d026061d3a4a70260ac1a1186fb5da4
jango <headrichjan@gmail.com>
2026-05-06 01:28:07 +0800

    added 7-3_multiple_of_ten.py

---
+++ 7-3_multiple_of_ten.py
@@ -0,0 +1,7 @@
+number_question = input("Give me a number = ")
+number_question = int(number_question)
+
+if number_question % 10 == 0:
+    print("This number is a multiple 10. Great Job!")
+else:
+    print("This number is not a multiple 10.")
