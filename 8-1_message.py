2569a4eeb74c15917141e63136f5f54db9e3382b
jango <headrichjan@gmail.com>
2026-05-08 13:11:33 +0800

    added 8-1_message.py

---
+++ 8-1_message.py
@@ -0,0 +1,6 @@
+def display_message():
+    """A sentence telling everyone what I will be learning in this chapter."""
+    print("Hi everyone, I will be learning about FUNCTIONS in this chapter.")
+
+
+display_message()
