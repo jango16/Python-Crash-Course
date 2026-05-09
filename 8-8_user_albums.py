6c6abc9c2638d4887175e505184fbe85f88f0b6a
jango <headrichjan@gmail.com>
2026-05-10 05:49:03 +0800

    added 8-8_user_albums.py

---
+++ 8-8_user_albums.py
@@ -0,0 +1,23 @@
+def make_album(album, artist_name, album_title):
+    """Return an artist album as a dictionary."""
+    album['artist'].append(artist_name)
+    album['album'].append(album_title)
+    return album
+
+
+album = {'artist': [], 'album': []}
+
+while True:
+    print("\nGive me your favorite artist and their album:")
+    print("(enter 'q' at any prompt to exit)\n")
+
+    name_artist = input('Artist Name: ')
+    if name_artist == 'q':
+        break
+
+    name_album = input('Album Title: ')
+    if name_album == 'q':
+        break
+
+    formatted_album = make_album(album, name_artist, name_album)
+    print(formatted_album)
