c3d8dd78797aa9b45bcfe5b48d51cbce0e95cde3
jango <headrichjan@gmail.com>
2026-05-10 12:14:29 +0800

    added ex8_11_archived_messages.py

---
+++ ex8_11_archived_messages.py
@@ -0,0 +1,24 @@
+def send_messages(message, sent):
+    """prints and move each message from a list to another"""
+    while message:
+        msg = message.pop(0)
+        print(msg)
+        sent.append(msg)
+
+
+short_text_messages = [
+    'Hey, just checking in. Hope your day is going well.',
+    'I saw something today that reminded me of you.',
+    'Do not forget to take a break and drink some water.',
+    'You have been on my mind lately.',
+    'Hope something good happens to you today.'
+]
+
+sent_messages = []
+
+send_messages(short_text_messages[:], sent_messages)
+
+print('\nOriginal List:')
+print(short_text_messages)
+print('\nSent List:')
+print(sent_messages)
